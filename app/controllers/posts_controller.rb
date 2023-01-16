d06bfae9cfe796f9ccb7c51b9c83d164b6e1a012
crane3216 <turumi3216@gmail.com>
2023-01-16 20:16:48 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
 
   def index
-    @ponnko = "これはコントローラーで定義したインスタンス変数を確認するための文字列"
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
   end
 
 end
